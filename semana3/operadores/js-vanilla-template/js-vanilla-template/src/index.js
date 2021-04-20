4656677b46ee9fe935f8ddef34b9d519a1931766
Drilias <drica.e.f@hotmail.com>
2021-04-20 17:38:20 -0300

    Respondendo aos exercícios

--- semana3/operadores/js-vanilla-template/js-vanilla-template/src/index.js
+++ semana3/operadores/js-vanilla-template/js-vanilla-template/src/index.js
@@ -34,7 +34,7 @@ const numeroPar = Number (prompt ('Digite um número par:'))
 
 //b.
 const restoDivisao = numeroPar % 2
-.console.log('o resto da divisão do número digitado por 2=', restoDivisao)
+console.log('o resto da divisão do número digitado por 2=', restoDivisao)
 
 //c.
 //Como se tratam de números pares os restos da divisão por 2 são todos iguais a zero.
@@ -43,3 +43,30 @@ const restoDivisao = numeroPar % 2
 //Se digitar número ímpar o resto é diferente de zero.
 
 //Exercício 3
+//a.
+const listaDeTarefas = []
+
+//b.
+const primeiraTarefa = prompt('Informe a primeira tarefa que você precise realizar no dia:')
+const segundaTarefa = prompt('Informe a segunda tarefa que você precise realizar no dia:')
+const terceiraTarefa = prompt('Informe a terceira tarefa que você precise realizar no dia:')
+const listaDeTarefas = ['primeiraTarefa', 'segundaTarefa', 'terceiraTarefa']
+
+//c.
+console.log(listaDeTarefas)
+
+//d.
+const indiceTarefa = Number (prompt('Digite o índice de uma tarefa que você realizou:', 0, 1 ou 2))
+
+//e.
+array.splice(2)
+
+//f.
+console.log(listaDeTarefas)
+
+//4.
+const nomeUsuario = prompt('Digite seu nome:')
+const emailDoUsuario = prompt('Digite seu e-mail:')
+
+const mensagem = 'O e-mail' + emailDoUsuario + 'foi cadastrado com sucesso. Seja bem-vinda(o),' + nomeDoUsuario'!'
+console.log(mensagem)

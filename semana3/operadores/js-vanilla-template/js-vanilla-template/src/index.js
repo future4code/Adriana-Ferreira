9c33d63b5b4965209b7ffd8c3916955d6f85101a
Drilias <drica.e.f@hotmail.com>
2021-04-20 16:49:47 -0300

    Respostas do exercício

---
+++ semana3/operadores/js-vanilla-template/js-vanilla-template/src/index.js
@@ -0,0 +1,45 @@
+//Exercícios de interpretação de código
+
+/*Exercício 1
+
+a. false
+b. false
+c. true
+e. boolean */
+
+/*Exercício 2
+
+a. undefined
+b. null
+c. 10
+d. 0
+e. 19
+f. 25 */
+
+//Exercícios de escrita de código
+
+//Exercício 1
+//a.
+const idadeUsuario = Number (prompt ('Digite sua idade:'))
+const idadeAmiga = Number (prompt('Amiga, qual é a sua idade?'))
+//b
+console.log("Sua idade é maior do que a do seu melhor amigo?", true || false)
+//c.
+const somaDasIdades = idadeUsuario - idadeAmiga
+console.log('Subtração das idades=', somaDasIdades)
+
+//Exercício 2
+//a.
+const numeroPar = Number (prompt ('Digite um número par:'))
+
+//b.
+const restoDivisao = numeroPar % 2
+.console.log('o resto da divisão do número digitado por 2=', restoDivisao)
+
+//c.
+//Como se tratam de números pares os restos da divisão por 2 são todos iguais a zero.
+
+//d.
+//Se digitar número ímpar o resto é diferente de zero.
+
+//Exercício 3

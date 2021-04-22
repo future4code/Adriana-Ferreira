d12bb01cb63280c6352d7399a41e0663db40f691
Drilias <drica.e.f@hotmail.com>
2021-04-22 16:48:25 -0300

    Respostas dos exercícios

---
+++ semana3/condicionais/index.js
@@ -0,0 +1,12 @@
+//Exercícios de interpretação de código
+//1. O código pede um número ao usuário e verifica se ele é par, se sim imprime mensagem
+//'Passou no teste' senão for par imprime 'Não passou no teste'
+
+//2.a) Serve para fazer compras de mercado numa plataforma como um site ou app eu imagino.
+//c) Acho que seria a mensagem impressa a mensagem mesmo, porque o usuário escolheu uma opção disponível.
+//O preço da fruta Pêra é R$ 5,50
+
+//3. a) Na primeira linha é solicitado ao usuário que digite um número. E é feita a conversão para número do dado digitado.
+//b) Esse número passou no teste.
+//Se fosse o número 10 não haveria mesnsagem.
+//c) Haverá erro, pois a variável mensagem não é global.

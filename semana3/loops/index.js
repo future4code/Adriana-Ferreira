f445f3ce672aa39b9fd2549f8763b162d6b6f59f
Drilias <drica.e.f@hotmail.com>
2021-04-23 16:46:39 -0300

    Respostas dos exercícios - Laços

---
+++ semana3/loops/index.js
@@ -0,0 +1,41 @@
+//Exercícios de interpretação de código
+/*EXERCÍCIO 1
+Na primeira linha é declarada a variável valor igual a 0
+Na segunda tem um laço for para a variável i=0, na qual na condição deve-se 
+incrementar 1 a i até i < 5 
+Na terceira linha o valor de i é somado à variável valor
+Na quarta linha tem um comando que imprimirá os resultados para a variável valor
+
+/*EXERCÍCIO 2
+a) 19, 21, 23, 25, 27, 30
+b) Sim, for...of... seria suficiente 
+
+var cores = ['vermelho', 'verde', 'azul'];
+for (var i = 0; i < cores.length; i++) {
+  console.log(cores[i]);
+}
+
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (var i = 0; i < lista.length; i++) {
+ 		console.log(lista[i]);	
+}
+
+//DESAFIO 1
+A mensagem seria: 
+0
+//Exercícios de escrita de código
+a) let original = [0,1,2,3,4,5,6]
+   console.log(original[i])
+
+b) let butterfly = [10, 11, 12, 15, 18]
+for (var i = 0; i < butterfly.length; i/10) {
+ 		console.log(lista[i]);	
+}
+
+c) let butterfly = [10, 11, 12, 15, 18]
+if (var i = 0; i % 2 = 0) {
+ 		console.log(lista[i]);	
+}
+
+d) 
+

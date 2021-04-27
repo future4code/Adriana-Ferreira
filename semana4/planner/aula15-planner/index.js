e9e1eb52512457c979541b58f3b6d3b9b9f101a6
Adriana <drica.e.f@hotmail.com>
2021-04-27 16:25:51 -0300

    Exercitando DOM

---
+++ semana4/planner/aula15-planner/index.js
@@ -0,0 +1,19 @@
+
+/*1)
+function printMessage() {
+console.log("hi")
+} 
+printMessage()*/
+
+
+//2)
+function locateInput() {  
+    const myInput = document.getElementById("tarefa")
+    console.log(myInput.value)    
+} 
+
+//3)
+function locateSelectValue() {  
+    const theSelected = document.getElementById("dias-semana")
+    console.log(theSelected.value)    
+} 

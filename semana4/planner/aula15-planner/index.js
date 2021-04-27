50ee5c3732b521cd4a0565f0c7e818dbb0756758
Adriana <drica.e.f@hotmail.com>
2021-04-27 19:26:49 -0300

    Respostas dos exercícios 27-04

--- semana4/planner/aula15-planner/index.js
+++ semana4/planner/aula15-planner/index.js
@@ -7,13 +7,33 @@ printMessage()*/
 
 
 //2)
-function locateInput() {  
-    const myInput = document.getElementById("tarefa")
-    console.log(myInput.value)    
+    function locateInput() {  
+    const myInput = document.getElementById("tarefa")   
 } 
 
 //3)
-function locateSelectValue() {  
+    function locateSelectValue() {  
+    const theSelected = document.getElementById("dias-semana") 
+} 
+
+//4)
+/*  function locateInput() {  
+    const myInput = document.getElementById("tarefa")
+    console.log(myInput.value)    
+} 
+    function locateSelectValue() {  
     const theSelected = document.getElementById("dias-semana")
     console.log(theSelected.value)    
+} */
+
+//5)
+
+   locateInput.innerHTML = "<div>'${theSelected.value}'</div>"
+
+//6)
+    function locateInput() {  
+    const myInput = document.getElementById("tarefa")
+    myInput.innerHTML=""
 } 
+
+

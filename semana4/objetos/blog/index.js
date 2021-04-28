eaad127e6ba686a6a9ea14d9aeebf0e4860cb373
Adriana <drica.e.f@hotmail.com>
2021-04-28 16:37:30 -0300

    Respondendo aos exercícios de objetos

---
+++ semana4/objetos/blog/index.js
@@ -0,0 +1,17 @@
+
+//1.
+//a.
+
+const formulario = document.getElementById('formulario').value;
+const tituloPost = document.getElementById('titulo-post').value;
+const autorPost = document.getElementById('autor-post').value;
+const conteudoPost = document.getElementById('conteudo-post').value;
+const containerPost = document.getElementById('container-de-posts').value;
+
+const post = {
+    titulo: tituloPost.value
+    autor: autorPost.value
+    conteudo: containerPost.value 
+}
+
+

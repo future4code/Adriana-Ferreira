5e08f6dcb2ab17d16d64ba62ca0bc33cefc93616
Drilias <drica.e.f@hotmail.com>
2021-06-03 22:46:10 -0300

    finalizando projeto

---
+++ semana9/astromatch/astromatch/src/components/MatchList/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const CardMatch = styled.div`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   border: 1px solid black;
+   height: 40vh;
+   width: 20vw;
+   `

1d52f0a5fc710f824cc5542a14fee517e5223a6f
Drilias <drica.e.f@hotmail.com>
2021-08-16 21:20:33 -0300

    exercicios aula58

---
+++ semana20/arquitetura-de-software/src/business/users/createUserBusiness.ts
@@ -0,0 +1,42 @@
+import { hash } from "bcryptjs";
+import { generateToken } from "../../services/authenticator";
+import { generateId } from "../../services/idGenerator";
+
+export const createUserBusiness = ({
+   id,
+   name,
+   email, 
+   password, 
+   role
+}:any) => {
+    if(
+      !name ||      
+      !email ||
+      !password ||
+      !role
+    ){
+       throw new Error('Preencha todos os campos')
+    }
+}
+
+const id: string = generateId()
+
+const cypherPassword = await hash(password);
+
+await insertUser({
+    id,
+    name,
+    email,
+    password: cypherPassword,
+    role
+})
+
+const token: string = generateToken({
+   id,
+   role: role
+})
+
+return token
+
+}
+

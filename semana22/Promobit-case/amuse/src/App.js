b1e8e49280f570630c7614d00beb91c6f3ea6751
Drilias <drica.e.f@hotmail.com>
2021-09-03 19:30:51 -0300

    criaçao dos componentes

--- semana22/Promobit-case/amuse/src/App.js
+++ semana22/Promobit-case/amuse/src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
-import Row from "./components/Row";
-import requests from "./services/requests"
+import Row from "./components/Row/Row";
+import requests from "./services/requests";
+import GlobalStyle from "./globalStyles/GlobalStyles";
+import Banner from "./components/Banner/Banner";
 
 function App() {
-  return (
-    <div>
-      <h1>amuse</h1>
-        <Row title="Originals" getUrl={requests.getOriginals}></Row>
-        <Row title="Trending now" getUrl={requests.getTrending}></Row>
-        <Row title="Top Rated" getUrl={requests.getTopRated}></Row>
-        <Row title="Comedy Movies" getUrl={requests.getComedyMovies}></Row>
-        <Row title="Romance Movies" getUrl={requests.getRomanceMovies}></Row>
-        <Row title="Horror Movies" getUrl={requests.getHorrorMovies}></Row>
-      <Row title="Documentaries" getUrl={requests.getDocumentaries}></Row>
-    </div>
-  );
+    return (
+        <div>
+            <Banner/>
+                <Row title="Originals" getUrl={requests.getOriginals} isLargeRow></Row>
+                <Row title="Trending now" getUrl={requests.getTrending}></Row>
+                <Row title="Top Rated" getUrl={requests.getTopRated}></Row>
+                <Row title="Comedy Movies" getUrl={requests.getComedyMovies}></Row>
+                <Row title="Romance Movies" getUrl={requests.getRomanceMovies}></Row>
+                <Row title="Horror Movies" getUrl={requests.getHorrorMovies}></Row>
+                <Row title="Documentaries" getUrl={requests.getDocumentaries}></Row>
+            <GlobalStyle />
+        </div>
+    );
 }
 
 export default App;

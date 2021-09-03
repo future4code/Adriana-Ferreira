3b831751c7d8488c57deb77d35f2f61527870647
Drilias <drica.e.f@hotmail.com>
2021-09-02 23:06:25 -0300

    configuraçoes iniciais

---
+++ semana22/Promobit-case/amuse/src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div>
+      Hey, Adriana Elias Ferreira! You are the best software engineer of the world!
+    </div>
+  );
+}
+
+export default App;
+
+//Chave da API (v3 auth)  
+//bbb3c5956c9ee54b49b8031bbbffd77b
+
+//Exemplo de Requisição de API 
+//https://api.themoviedb.org/3/movie/550?api_key=bbb3c5956c9ee54b49b8031bbbffd77b
+
+//https://amuse-812a0.web.app
